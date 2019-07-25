28bbe75df94e7e243f08be57b875f27bc5211015
Bar Snir <bars@yad2.co.il>
2019-07-25 17:09:46 +0300

    added Exec1 in Array Structure

---
+++ DataStructures/Arrays/Exec1.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+
+function getTwoSumIndices(){
+    const numbers = [2, 7, 11, 15];
+    const target = 9;
+    const indicesArr = [];
+
+    for ( let i = 0; i < numbers.length; i++) {
+
+    }
+}
