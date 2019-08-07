8f0ffecc3fc78c8b49ce9104b3c3f63a4c395136
Bar Snir <bars@yad2.co.il>
2019-08-07 16:49:50 +0300

    finished with queue stracture

--- DataStructures/StacksAndQueues/Queue.js
+++ DataStructures/StacksAndQueues/Queue.js
@@ -1,36 +1,48 @@
 class Node {
-    constructor(value){
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
-  
-class Stack {
-    constructor(){
-      this.top = null;
-      this.bottom = null;
-      this.length = 0;
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value){
+    const newNode = new Node(value);
+    if (this.length === 0 ) {
+       this.first = newNode;
+       this.last = newNode; 
     }
-    peek() {
-        return this.top;
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
+  dequeue(){
+    if(!this.first){
+      return null;
     }
-    push(value) {
-        const newNode = new Node(value);
-        if(this.bottom === null) {
-            this.top = newNode;
-            this.bottom = newNode;
-            return this;
-        }
-        const holdingPointer = this.top;
-        this.top = newNode;
-        this.top.next = holdingPointer;
-        this.length ++;
-        return this;
+    if(this.first === this.last){
+      this.last = null;
+      this.length--;
     }
-    pop() {
-        this.length --;
-    }
-}  
-const myStack = new Stack();
-myStack.push('google');
-console.log(myStack);
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue);
